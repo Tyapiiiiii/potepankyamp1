e7060bb9ce53caffa3f38299eff9221b526f2ef8
Tyapiiiiii <halloween.fu@icloud.com>
2026-04-24 20:17:20 +0900

    button

--- JSTask1/main.js
+++ JSTask1/main.js
@@ -2,32 +2,61 @@ let startTime;
 let elapsedTime = 0;
 let timerInterval;
 
-function start() {
-  clearInterval(timerInterval);
-  startTime = Date.now() - elapsedTime;
 
-  timerInterval = setInterval(() => {
-    elapsedTime = Date.now() - startTime;
+$(document).ready(function() {
+  $('.start').click(function() {
+    $(this).prop("disabled", true);
+    $('.stop').prop("disabled", false);
+    $('.reset').prop("disabled", false);
+    start();
+  });
 
-    const h = String(Math.floor(elapsedTime / 3600000)).padStart(2, '0');
-    const m = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(2, '0');
-    const s = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(2, '0');
-    const ms = String(Math.floor((elapsedTime % 1000) / 10)).padStart(2, '0');
+  function start() {
+    clearInterval(timerInterval);
+    startTime = Date.now() - elapsedTime;
 
-    document.getElementById('time').textContent = `${h}:${m}:${s}.${ms}`;
-  }, 10);
-  console.log("タイマーがスタートしました");
-}
+    timerInterval = setInterval(() => {
+      elapsedTime = Date.now() - startTime;
 
-function stop() {
-  clearInterval(timerInterval);
-  console.log("タイマーがストップしました");
-}
+      const h = String(Math.floor(elapsedTime / 3600000)).padStart(2, '0');
+      const m = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(2, '0');
+      const s = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(2, '0');
+      const ms = String(Math.floor((elapsedTime % 1000) / 10)).padStart(2, '0');
 
-function reset() {
+      document.getElementById('time').textContent = `${h}:${m}:${s}.${ms}`;
+    }, 10);
+    console.log("タイマーがスタートしました");
+  }
+});
+
+
+$(document).ready(function() {
+  $('.stop').click(function() {
+    $(this).prop("disabled", true);
+    $('.start').prop("disabled", false);
+    $('.reset').prop("disabled", false);
+    stop();
+  });
+
+  function stop() {
+    clearInterval(timerInterval);
+    console.log("タイマーがストップしました");
+  }
+});
+
+
+$(document).ready(function() {
+  $('.reset').click(function() {
+    $(this).prop("disabled", true);
+    $('.start').prop("disabled", false);
+    $('.stop').prop("disabled", true);
+    reset();
+  });
+
+  function reset() {
   clearInterval(timerInterval);
   elapsedTime = 0;
   document.getElementById('time').textContent = "00:00:00.00";
   console.log("タイマーがリセットされました");
-}
-
+  }
+});
